8ea66dab35d680b0d9177d8c78962c6d478cfe9d
Mateus T <mateustavarestv@hotmail.com>
2023-01-24 21:27:04 -0300

    teste

---
+++ r2a/R2ANewAlgoritm1.py
@@ -0,0 +1,18 @@
+from r2a.ir2a import IR2A
+
+
+class R2ANewAlgoritm1(IR2A):
+    def __int__(self, id):
+        IR2A.__init__(self, id)
+    def handle_xml_request(self, msg):
+        pass
+    def handle_xml_response(self, msg):
+        pass
+    def handle_segment_size_request(self, msg):
+        pass
+    def handle_segment_size_response(self, msg):
+        pass
+    def initialize(self):
+        pass
+    def finalization(self):
+        pass
